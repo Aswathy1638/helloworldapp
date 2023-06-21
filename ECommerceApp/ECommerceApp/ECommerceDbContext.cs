3066b3346c9148a5925a29bba5bb349443af16da
Aswathy1638 <aswathy1638@gmail.com>
2023-06-21 17:10:25 +0530

    commit

---
+++ ECommerceApp/ECommerceApp/ECommerceDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp
+{
+    public class ECommerceDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+        }
+    }
+    
+}
